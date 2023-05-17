fba618940281114c0630e619cbae37c6ff333d62
Peter Holm <peter@l-holm.dk>
2023-05-17 18:44:20 +0200

    check

--- split2scan/scan_stl.py
+++ split2scan/scan_stl.py
@@ -4,6 +4,7 @@ import open3d as o3d
 
 
 def show_mesh(mesh, axis=True):
+    "Show Mesh"
     meshlist = [mesh]
     if axis:
         axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.01, origin=[0, 0, 0])
@@ -11,7 +12,7 @@ def show_mesh(mesh, axis=True):
 
     if not mesh.has_triangle_normals():
         mesh.compute_triangle_normals()
-    o3d.visualization.draw_geometries(meshlist, window_name="showcrop", width=800, height=600)  
+    o3d.visualization.draw_geometries(meshlist, window_name="showcrop", width=800, height=600)
                                 # zoom=_ZOOM,
                                 # front=_FRONT,
                                 # lookat=_LOOKAT,
@@ -20,6 +21,7 @@ def show_mesh(mesh, axis=True):
                                 # mesh_show_back_face=True,
                                 # mesh_show_wireframe=False)
 def show_pcl(pcl, axis=True):
+    "Show pcl"
     meshlist = []
     if axis:
         axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.01, origin=[0, -0.0, 0.0])
@@ -28,7 +30,7 @@ def show_pcl(pcl, axis=True):
     if not pcl.has_normals():
         pcl.compute_normals()
     meshlist.append(pcl)
-    o3d.visualization.draw_geometries(meshlist, window_name="showcrop", width=800, height=600)  
+    o3d.visualization.draw_geometries(meshlist, window_name="showcrop", width=800, height=600)
                                 # zoom=_ZOOM,
                                 # front=_FRONT,
                                 # lookat=_LOOKAT,
@@ -38,44 +40,47 @@ def show_pcl(pcl, axis=True):
                                 # mesh_show_wireframe=False)
 
 def center_mesh(mesh):
-    center = mesh.get_center()
+    "center the mesh around 0,0,0"
     new = mesh.translate((0,0,0), relative=False)
     return new
 
 def stl2pcl(stl):
+    "convert mesh to pointcloud"
     pcl = stl.sample_points_uniformly(10000)
     return pcl
 
 def hide_point(pcl, camera=(0,-0.1, 0), radius=100.1):
+    "hide points seen from camera"
     _, pt_map = pcl.hidden_point_removal(camera, radius)
     pcd = pcl.select_by_index(pt_map)
     show_pcl(pcd)
     return pcd
- 
+
 def scan_mesh(mesh, positions, filename):
+    "filter mesh from given positions"
     nr=1
     for p in positions:
         pcl = stl2pcl(mesh)
         #show_pcl(pcl)
         print("camera", p)
-        m = hide_point(pcl, camera=p)
-        show_pcl(m)
+        p_view = hide_point(pcl, camera=p)
+        show_pcl(p_view)
         filen = filename + str(nr) + ".ply"
-        o3d.io.write_point_cloud(filen, m)
+        o3d.io.write_point_cloud(filen, p_view)
         nr += 1
 
 if __name__=="__main__":
     INFIL = Path('testdata/stl/tooth/two_tooth/fortand.stl')
     OUTPATH = INFIL.parent
 
-    mesh = o3d.io.read_triangle_mesh(str(INFIL))
+    mymesh = o3d.io.read_triangle_mesh(str(INFIL))
     #show_mesh(mesh)
-    cmesh = center_mesh(mesh)
+    cmesh = center_mesh(mymesh)
     mypcl = stl2pcl(cmesh)
     o3d.io.write_point_cloud('testdata/stl/tooth/two_tooth/fortand.ply', mypcl)
     #show_mesh(cmesh)
-    positions = [(0, -0.1, 0),(0.0, -0.0, -10.1)]
-    scan_mesh(cmesh, positions, "file")
+    mypositions = [(0, -0.1, 0),(0.0, -0.0, -10.1)]
+    scan_mesh(cmesh, mypositions, "file")
 
     # o3d.io.write_point_cloud(str(OUTPATH / 'crop.ply')
     # pcl = stl2pcl(crop)
