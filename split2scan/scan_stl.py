ccdf714dc3c9b1e1c617cb0e94ae8b95e80c4f77
Peter Holm <peter@l-holm.dk>
2024-03-06 10:02:15 +0100

    Updated angles and color

--- split2scan/scan_stl.py
+++ split2scan/scan_stl.py
@@ -8,7 +8,12 @@ from copy import deepcopy
 from pathlib import Path
 import open3d as o3d
 
-DEBUG = True
+POINT_PR_M2 = 160 * 160 * 100 * 100
+FULL_RES_POINTS = 1000000
+PAINT_COLOR = True
+
+DEBUG = False
+
 def show_mesh(mesh, axis=True, name="showmesh"):
     "Show Mesh"
     meshlist = [mesh]
@@ -50,9 +55,20 @@ def center_mesh(mesh):
     new = mesh.translate((0,0,0), relative=False)
     return new
 
-def stl2pcl(stl):
-    "convert mesh to pointcloud"
-    pcl = stl.sample_points_uniformly(10000)
+def stl2pcl(stl, number=None, method="uniformly"):
+    "convert mesh to pointcloud with number samples, number defaults to 160x160 points pr cm2"
+    area = stl.get_surface_area()
+    if number is None:
+        no_points = int(POINT_PR_M2 * area)
+    else:
+        no_points = number
+    print("Surface area", area, "no points", no_points)
+    if method=="uniformly":
+        pcl = stl.sample_points_uniformly(no_points)
+    elif method=="poisson":
+        pcl = stl.sample_points_poisson_disk(no_points)
+    else:
+        raise NotImplementedError("stl2pcl must be call with method==poisson or uninformly")
     return pcl
 
 def hide_point(pcl, camera=(0,-0.1, 0), radius=1.1):
@@ -65,36 +81,50 @@ def hide_point(pcl, camera=(0,-0.1, 0), radius=1.1):
 
 def scan_mesh(mesh, positions, filename):
     "filter mesh from given positions"
+    color = [ [0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1],
+                [0.5,0,0],[0.5,0,1],[0.5,1,0],[0.5,1,1],[0.5,0,0],[0.5,0,0.5],[0.5,0.5,0],[0.5,0.5,0.5],
+                [0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1],
+                [0.5,0,0],[0.5,0,1],[0.5,1,0],[0.5,1,1],[0.5,0,0],[0.5,0,0.5],[0.5,0.5,0],[0.5,0.5,0.5],
+    ]
     nr=1
-    orgpcl = stl2pcl(mesh)
+    opcl = stl2pcl(mesh)
     for p in positions:
-        pcl = deepcopy(orgpcl)
+        pcl = deepcopy(opcl)
+        if PAINT_COLOR:
+            pcl.paint_uniform_color(color[nr-1])
         #show_pcl(pcl)
         print("camera", p)
         p_view = hide_point(pcl, camera=p)
         #show_pcl(p_view, name="camera "+str(p))
         filen = filename + str(nr) + ".ply"
         o3d.io.write_point_cloud(filen, p_view)
+        print("Number of points", len(p_view.points))
         nr += 1
 
 if __name__=="__main__":
-    
     INFIL = Path('testdata/stl/tooth/two_tooth/fortand.stl')
     OUTPATH = INFIL.parent
 
     mymesh = o3d.io.read_triangle_mesh(str(INFIL))
-    if DEBUG:
-        show_mesh(mymesh, name="Original mesh")
-    cmesh = center_mesh(mymesh)
     # if DEBUG:
-    #     show_mesh(cmesh, name="Centered mesh")
-    mypcl = stl2pcl(cmesh)
+    #     show_mesh(mymesh, name="Original mesh")
+    cmesh = center_mesh(mymesh)
     if DEBUG:
-        show_pcl(mypcl, name="original PCL")
-        print("size", mypcl.get_min_bound(), mypcl.get_max_bound())
-    o3d.io.write_point_cloud('testdata/stl/tooth/two_tooth/fortand.ply', mypcl)
+        show_mesh(cmesh, name="Centered mesh")
+    orgpcl = stl2pcl(cmesh, 1000000)
+    if DEBUG:
+        show_pcl(orgpcl, name="original PCL")
+        print("size", orgpcl.get_min_bound(), orgpcl.get_max_bound())
+    o3d.io.write_point_cloud('testdata/stl/tooth/two_tooth/fortand.ply', orgpcl)
+    mypcl = stl2pcl(cmesh,1000)
 
-    mypositions = [(0, -0.01, 0),(-0.01, -0.01, 0.0),(+0.01, -0.01, 0.0),(0, -0.00, -0.01),(-0.01, -0.00, -0.01),(+0.01, -0.00, -0.01),(0, -0.01, 0.0),(-0.01, -0.01, 0.0),(+0.01, -0.01, 0.0)]
+    mypositions = [(0, -0.01, 0),(-0.005, -0.01, 0.0),(-0.01, -0.01, 0.0),
+                   (+0.005, -0.01, 0.0),(+0.01, -0.01, 0.0),
+                   (-0.01, -0.01, -0.02),(-0.005, -0.01, -0.02),(+0.0, -0.01, -0.02),(0.005, -0.01, -0.02),(0.01, -0.01, -0.02),
+                   (-0.01, -0.005, -0.02),(-0.005, -0.005, -0.02),(+0.0, -0.005, -0.02),(0.005, -0.005, -0.02),(0.01, -0.005, -0.02),
+                   (-0.01, -0.00, -0.02),(-0.005, -0.00, -0.02),(+0.0, -0.00, -0.02),(0.005, -0.00, -0.02),(0.01, -0.00, -0.02),
+                   (-0.01, -0.00, -0.01),(-0.005, -0.00, -0.01),(+0.0, -0.00, -0.01),(0.005, -0.00, -0.01),(0.01, -0.00, -0.01),
+                   ]
     scan_mesh(cmesh, mypositions, "tmp/file")
 
     # o3d.io.write_point_cloud(str(OUTPATH / 'crop.ply')
