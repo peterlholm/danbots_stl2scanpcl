e4d1839064a27d227d2bbfe6b350b134bd6f6c43
Peter Holm <peter@l-holm.dk>
2024-03-08 14:12:20 +0100

    check ok

--- split2scan/scan_stl.py
+++ split2scan/scan_stl.py
@@ -7,22 +7,43 @@ generate new pointclods
 from copy import deepcopy
 from pathlib import Path
 import open3d as o3d
+import numpy as np
+
 
 POINT_PR_M2 = 160 * 160 * 100 * 100
 FULL_RES_POINTS = 1000000
 PAINT_COLOR = True
+MAX_DISTANCE = 0.135
 
-DEBUG = False
+DEBUG = True
 
-def show_mesh(mesh, axis=True, name="showmesh"):
+def point_distance(p1, p2):
+    "compute distance between 2 points"
+    dist = np.linalg.norm(np.array(p1)-np.array(p2))
+    print ("dist ", dist)
+
+def remove_point_distance(pcl, pkt, max_dist=1):
+    "Remove all pont in pointcloud where distance > max_dist"
+    npkt = np.asarray(pkt)
+    points = np.asarray(pcl.points)
+    pcl1 = o3d.geometry.PointCloud()
+    distarr = np.linalg.norm(points - npkt, axis=1)
+    pcl1.points = o3d.utility.Vector3dVector(points[distarr <= max_dist ])
+    if pcl.has_colors():
+        colors = np.asarray(pcl.colors)
+        pcl1.colors = o3d.utility.Vector3dVector(colors[distarr <= max_dist ])
+    print(f"removed {len(pcl.points) - len(pcl1.points)} points")
+    return pcl1
+
+def show_mesh(meshlist, axis=True, name="showmesh"):
     "Show Mesh"
-    meshlist = [mesh]
+    #meshlist = [mesh]
     if axis:
         axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.01, origin=[0, 0, 0])
         meshlist.append(axis_pcd)
-
-    if not mesh.has_triangle_normals():
-        mesh.compute_triangle_normals()
+    for m in meshlist:
+        if not m.has_triangle_normals():
+            m.compute_triangle_normals()
     o3d.visualization.draw_geometries(meshlist, window_name=name, width=800, height=600)
                                 # zoom=_ZOOM,
                                 # front=_FRONT,
@@ -31,17 +52,46 @@ def show_mesh(mesh, axis=True, name="showmesh"):
                                 # point_show_normal=False,
                                 # mesh_show_back_face=True,
                                 # mesh_show_wireframe=False)
-def show_pcl(pcl, axis=True, name="showpcl"):
+
+def show_pcl(meshlist, axis=True, name="showpcl"):
     "Show pcl"
-    meshlist = []
+    #meshlist = []
+    for p in meshlist:
+        if not p.has_normals():
+            p.estimate_normals()
+            #pcl.compute_normals()
+    #meshlist.append(pcl)
     if axis:
         axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.01, origin=[0, -0.0, 0.0])
         meshlist.append(axis_pcd)
 
-    if not pcl.has_normals():
-        pcl.compute_normals()
-    meshlist.append(pcl)
-    o3d.visualization.draw_geometries(meshlist, window_name=name, width=800, height=600)
+    o3d.visualization.draw_geometries(meshlist, window_name=name, width=800, height=600,
+                               
+                                )
+                                # point_show_normal=False,
+                                # mesh_show_back_face=True,
+                                # mesh_show_wireframe=False)
+    
+
+def show_pcl2(meshlist, axis=True, name="showpcl", position=None):
+    "Show pcl"
+    #meshlist = []
+    for p in meshlist:
+        if not p.has_normals():
+            p.estimate_normals()
+            #pcl.compute_normals()
+    #meshlist.append(pcl)
+    if axis:
+        axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.01, origin=[0, -0.0, 0.0])
+        meshlist.append(axis_pcd)
+    if position is None:
+        position=(0,0,5)
+    
+    o3d.visualization.draw_geometries(meshlist, window_name=name, width=800, height=600,
+                                zoom=0.7,
+                                front=position,
+                                lookat=(0,0,0),
+                                up=(0,1,0))
                                 # zoom=_ZOOM,
                                 # front=_FRONT,
                                 # lookat=_LOOKAT,
@@ -71,12 +121,10 @@ def stl2pcl(stl, number=None, method="uniformly"):
         raise NotImplementedError("stl2pcl must be call with method==poisson or uninformly")
     return pcl
 
-def hide_point(pcl, camera=(0,-0.1, 0), radius=1.1):
+def hide_point(pcl, camera=(0,-0.1, 0), radius=10.1):
     "hide points seen from camera"
     _, pt_map = pcl.hidden_point_removal(camera, radius)
     pcd = pcl.select_by_index(pt_map)
-    if DEBUG:
-        show_pcl(pcd, name="HIDE " + str(camera))
     return pcd
 
 def scan_mesh(mesh, positions, filename):
@@ -90,40 +138,53 @@ def scan_mesh(mesh, positions, filename):
     opcl = stl2pcl(mesh)
     for p in positions:
         pcl = deepcopy(opcl)
-        if PAINT_COLOR:
-            pcl.paint_uniform_color(color[nr-1])
+        p_view = hide_point(pcl, camera=p)
         #show_pcl(pcl)
         print("camera", p)
-        p_view = hide_point(pcl, camera=p)
-        #show_pcl(p_view, name="camera "+str(p))
-        filen = filename + str(nr) + ".ply"
+        newpcl = remove_point_distance(p_view, p, max_dist=MAX_DISTANCE)
+        if PAINT_COLOR:
+            newpcl.paint_uniform_color(color[nr-1])
+        show_pcl2([p_view, newpcl], name="camera "+str(p), position=p)
+        filen = f"{filename}{nr:02}.ply"
         o3d.io.write_point_cloud(filen, p_view)
         print("Number of points", len(p_view.points))
         nr += 1
 
 if __name__=="__main__":
-    INFIL = Path('testdata/stl/tooth/two_tooth/fortand.stl')
-    OUTPATH = INFIL.parent
+    INFIL = Path('testdata/stl/tooth/two_tooth/fortand_r.stl')
+    OUTPATH = Path(__file__).parent.parent / "tmp"
 
+    #point_distance((0,0,0), (0.01, 0.01, 0.01))
     mymesh = o3d.io.read_triangle_mesh(str(INFIL))
     # if DEBUG:
     #     show_mesh(mymesh, name="Original mesh")
     cmesh = center_mesh(mymesh)
-    if DEBUG:
-        show_mesh(cmesh, name="Centered mesh")
     orgpcl = stl2pcl(cmesh, 1000000)
     if DEBUG:
-        show_pcl(orgpcl, name="original PCL")
-        print("size", orgpcl.get_min_bound(), orgpcl.get_max_bound())
+        show_mesh([cmesh], name="Centered mesh")
+        o3d.io.write_triangle_mesh(str(OUTPATH / 'center.ply'),cmesh)
+        #show_pcl(orgpcl, name="original PCL")
+        print(f"size  min: {orgpcl.get_min_bound()} Max: {orgpcl.get_max_bound()}")
     o3d.io.write_point_cloud('testdata/stl/tooth/two_tooth/fortand.ply', orgpcl)
     mypcl = stl2pcl(cmesh,1000)
+    dx= 0.006
+    Y=-0.000
+    dy=-0.01
+    Z = 0.019
+    dz= -0.007
+    y=-0.015
+    d=0.01
+    mypositions = [(-dx*2, Y, Z), (-dx, Y, Z),  (0.00, Y, Z), (dx, Y, Z), (2*dx, Y, Z), # 1-5
+                    (-dx*2, Y+dy, Z), (-dx, Y+dy, Z),  (0.00, Y+dy, Z), (+dx, Y+dy, Z), (2*dx, Y+dy, Z),
+                    (-dx*2, Y+dy, Z+dz), (-dx, Y+dy, Z+dz),  (0.00, Y+dy, Z+dz), (+dx, Y+dy, Z+dz), (2*dx, Y+dy, Z+dz),
+                    (-dx*2, Y+2*dy, Z+2*dz), (-dx, Y+2*dy, Z+2*dz),  (0.00, Y+2*dy, Z+2*dz), (+dx, Y+2*dy, Z+2*dz), (2*dx, Y+2*dy, Z+2*dz),
+                    (-dx*2, Y+dy, Z+3*dz), (-dx, Y+dy, Z+3*dz),  (0.00, Y+dy, Z+3*dz), (+dx, Y+dy, Z+3*dz), (2*dx, Y+dy, Z+3*dz),
+                    (-dx*2, Y, Z+3*dz), (-dx, Y, Z+3*dz),  (0.00, Y, Z+3*dz), (+dx, Y, Z+3*dz), (2*dx, Y, Z+3*dz),
 
-    mypositions = [(0, -0.01, 0),(-0.005, -0.01, 0.0),(-0.01, -0.01, 0.0),
-                   (+0.005, -0.01, 0.0),(+0.01, -0.01, 0.0),
-                   (-0.01, -0.01, -0.02),(-0.005, -0.01, -0.02),(+0.0, -0.01, -0.02),(0.005, -0.01, -0.02),(0.01, -0.01, -0.02),
-                   (-0.01, -0.005, -0.02),(-0.005, -0.005, -0.02),(+0.0, -0.005, -0.02),(0.005, -0.005, -0.02),(0.01, -0.005, -0.02),
-                   (-0.01, -0.00, -0.02),(-0.005, -0.00, -0.02),(+0.0, -0.00, -0.02),(0.005, -0.00, -0.02),(0.01, -0.00, -0.02),
-                   (-0.01, -0.00, -0.01),(-0.005, -0.00, -0.01),(+0.0, -0.00, -0.01),(0.005, -0.00, -0.01),(0.01, -0.00, -0.01),
+                #    (-0.01, -0.01, -0.02),(-0.005, -0.01, -0.02),(0.00, -0.01, -0.02),(0.005, -0.01, -0.02),(0.01, -0.01, -0.02), #6-10
+                #    (-0.01, -0.007, -0.02),(-0.005, -0.007, -0.02),(+0.0, -0.007, -0.02),(0.007, -0.005, -0.02),(0.01, -0.007, -0.02), #11-15
+                #    (-0.01, -0.00, -0.02),(-0.005, -0.00, -0.02),(+0.0, -0.00, -0.02),(0.005, -0.00, -0.02),(0.01, -0.00, -0.02), # 16-20
+                #    (-0.01, -0.00, -0.01),(-0.005, -0.00, -0.01),(+0.0, -0.00, -0.01),(0.005, -0.00, -0.01),(0.01, -0.00, -0.01), #21- 25
                    ]
     scan_mesh(cmesh, mypositions, "tmp/file")
 
